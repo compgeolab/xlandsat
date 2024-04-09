a0f43954a17ec9b875c6a610d9822e8dec41df82
Leonardo Uieda <leo@uieda.com>
2024-04-09 17:24:03 -0300

    Add an option to download L1 from Momotombo
    The L1 scene looks better and doesn't have the many atmospheric
    correction issues with the L2 one.

--- xlandsat/tests/test_datasets.py
+++ xlandsat/tests/test_datasets.py
@@ -20,21 +20,40 @@ from ..datasets import (
 
 
 @pytest.mark.parametrize("untar", [False, True], ids=["archive", "folder"])
-def test_fetching_functions(untar):
-    "Check that the download functions work"
-    functions = [
+@pytest.mark.parametrize(
+    "fetcher",
+    [
         fetch_brumadinho_after,
         fetch_brumadinho_before,
         fetch_liverpool,
         fetch_liverpool_panchromatic,
         fetch_manaus,
-        fetch_momotombo,
         fetch_roraima,
-    ]
-    for func in functions:
-        path = pathlib.Path(func(untar=untar))
-        assert path.exists()
-        if untar:
-            assert path.is_dir()
-        else:
-            assert not path.is_dir()
+    ],
+)
+def test_fetching_functions(untar, fetcher):
+    "Check that the download functions work"
+    path = pathlib.Path(fetcher(untar=untar))
+    assert path.exists()
+    if untar:
+        assert path.is_dir()
+    else:
+        assert not path.is_dir()
+
+
+@pytest.mark.parametrize("untar", [False, True], ids=["archive", "folder"])
+@pytest.mark.parametrize("level", [1, 2], ids=["L1", "L2"])
+@pytest.mark.parametrize(
+    "fetcher",
+    [
+        fetch_momotombo,
+    ],
+)
+def test_fetching_functions_levels(untar, level, fetcher):
+    "Check that the download functions work"
+    path = pathlib.Path(fetcher(untar=untar, level=level))
+    assert path.exists()
+    if untar:
+        assert path.is_dir()
+    else:
+        assert not path.is_dir()
